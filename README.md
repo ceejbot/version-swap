b7f7243d128606f1f7cd4a9a9ea98b0af781103d
C J Silverio <ceejceej@gmail.com>
2023-12-07 17:19:01 -0800

    Implement the versions directory setup checker.
    This required setting up logging so I can cheat and use
    log levels to implement quiet and verbose level output.
    I'm using `simplelog` as a sink, which in turn uses `paris`
    to do some simple html-to-ansi-color-codes things to add
    a little bit of styling to console messages. There is also
    a file that gets logged to that is not subject to the
    versbosity options and does more traditional timestamped
    logging without the colors.
    
    Implemented the core functions for walking directories and
    returning lists of relevant files.
    
    Expanded the command-line args a bit to support `--verbose` and
    `--quiet` as well as to optionally pass a game directory for
    both the check and the run commands..

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 `version-swap` is a command-line tool with a boring name that you use with your mod manager to swap versions of the Skryim executable when you launch the game. This allows you to have one game folder and run several different game versions. For example, you might want to run Skyrim version 1.5.97 so you can build grass caches with the [.Net Script Framework](https://www.nexusmods.com/skyrimspecialedition/mods/21294), then switch to version 1.6.1130 so you can have the latest animation native mods while you play.
 
-These instructions will help you get going with the tool so you can run any version of Skyrim you want to, without stress.
+These instructions help you get going with the tool so you can run any version of Skyrim you want to, without stress.
 
 ## Set up
 
@@ -12,11 +12,11 @@ __Before we start:__ These instructions assume you're modifying the game directo
 
 __STEP 1:__ Copy the `version-swap.exe` file into your game directory. Add it as a tool for your mod organizer. You'll want to make three different shortcuts for three different ways of running the tool:
 
-- `version-swap 1.6.1130`: Set up your game directory for version 1.6.1130 (the latest version at the time of this writing) and run it.
-- `version-swap 1.5.97`: Set up your game directory for 1.5.97 (or "SE") and run that.
+- `version-swap run 1.6.1130`: Set up your game directory for version 1.6.1130 (the latest version at the time of this writing) and run it.
+- `version-swap run 1.5.97`: Set up your game directory for 1.5.97 (or "SE") and run that.
 - `version-swap check`: Check to see if your directory set up has all the files required to make swapping work.
 
-We suggest you use the exact version numbers of the game instead of the "AE" and "SE" nicknames just in case you ever want to support more than one version of AE. If there's a new game version released, you can make a new directory for that version and set up a shortcut for it in your mod organizer. If you feel very silly, you can give these versions any name you like. If you feel like swapping between "skyrim-stormcloaks" and "skyrim-imperials", this tool will let you.
+We suggest you use the exact version numbers of the game instead of the "AE" and "SE" nicknames just in case you ever want to support more than one version of AE. If there's a new game version released, you can make a new directory for that version and set up a shortcut for it in your mod organizer. If you feel very silly, you can give these versions any name you like. If you feel like swapping between "skyrim-stormcloaks" and "skyrim-imperials", this tool lets you.
 
 __STEP 2:__ In your game root folder, put SKSE's dll for each of the game versions you're switching between. At the time of writing, those are `skse64_1_5_97.dll` and `skse64_1_6_1130.dll`. You can safely leave all SKSE dlls in the game folder no matter which version you want to run-- the loader executable is what determines which version of SKSE is used.
 
@@ -35,22 +35,22 @@ Pro tip: If a new version of Skyrim is released, you can support it with this to
 
 > ❓ We could also name this `skyrim-current` and `skyrim-legacy`. I am open to suggestions! I do like the idea of nudging people into learning the game versions. Also, the secret value of making the name include the game version is that we get support for swapping among N game versions for free.
 
-__STEP 4:__ In each of these subdirectories, make a `data` directory. We'll be stashing the game data that's different here, so the tool can swap them in and out. The new game version has new versions of two Creation Club mods: Fishing and Rare Curios. Copy the older versions of those two plugins into the data directory for 1.5.97, and the newer versions into the data directory for 1.6.1130. If you don't want to run those creations, leave them out of BOTH directories.
+__STEP 4:__ In each of these subdirectories, make a `data` directory. We'll be stashing the game data that's different here, so the tool can swap them in and out. The new game version has new versions of the four Creation Club mods, and the Fishing mod has a plugin with incompatible changes. Copy the older versions of `ccBGSSSE001-Fish.esm` into the data directory for 1.5.97, and the newer version into the data directory for 1.6.1130. If you don't want to run the fishing creation, leave it out of BOTH directories.
 
-__STEP 5:__ Make sure the empty `_ResourcePack.esl` plugin that came with this tool is in the `data` directory of the `1.5.97` game info. Copy the 1.6.1130 `_ResourcePack.esl` plugin into the matching `data` directory. We need to do this because older game versions will crash if they attempt to load that plugin file. You can safely leave the `_ResourcePack.bsa` file in place! The game won't do anything with it without the matching plugin.
+__STEP 5:__ Make sure the empty `_ResourcePack.esl` plugin that came with this tool is in the `data` directory of the `1.5.97` game info. Copy the 1.6.1130 `_ResourcePack.esl` plugin into the matching `data` directory. We need to do this because older game versions crash if they attempt to load that plugin file. You can safely leave the `_ResourcePack.bsa` file in place! The game won't do anything with it without the matching plugin.
 
-When you're done, your game folder or root mod should look like this:
+When you're done, your game folder (or root mod) should look like this:
 
 ```text
-Skyrim Special Edition
+.
+├── version-swap.exe
 ├── skse64_1_5_97.dll
 ├── skse64_1_6_1130.dll
 └── Versions
    ├── skyrim-1.5.97
    │  ├── data
    │  │  ├── _ResourcePack.esl
-   │  │  ├── ccBGSSSE001-Fish.esm
-   │  │  └── ccBGSSSE037-Curios.esl
+   │  │  └── ccBGSSSE001-Fish.esm
    │  ├── skse64_loader.exe
    │  ├── skse64_steam_loader.dll
    │  ├── SkyrimSE.exe
@@ -58,8 +58,7 @@ Skyrim Special Edition
    └── skyrim-1.6.1130
       ├── data
       │  ├── _ResourcePack.esl
-      │  ├── ccBGSSSE001-Fish.esm
-      │  └── ccBGSSSE037-Curios.esl
+      │  └── ccBGSSSE001-Fish.esm
       ├── skse64_loader.exe
       ├── SkyrimSE.exe
       └── steam_api64.dll
@@ -69,14 +68,11 @@ Skyrim Special Edition
 
 __STEP 5:__ Check your setup. Run `version-swap check` to verify that you've got everything in place. If the tool reports problems, follow the instructions it gives to fix things.
 
-## TODO
-
-- a flag to run without SKSE if asked
-- ???
-
 ## Building
 
 This is a Rust project. To build, start by installing the Rust toolchain with [rustup](https://rustup.rs) then run `cargo build`. There are some development conveniences available in the [justfile](https://just.systems). Run `cargo install just` to take advantage of them.
+
+The source is in a single file and quite short, but you can generate and read the usual programmer documentation by running `cargo doc --open`.
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::File;
+use std::path::PathBuf;
+
 use clap::{Parser, Subcommand};
 use eyre::{Report, Result};
-use owo_colors::OwoColorize;
+use log::LevelFilter;
+use simplelog::{ColorChoice, CombinedLogger, TermLogger, TerminalMode, WriteLogger};
 
 #[derive(Parser, Debug)]
 #[clap(name = "version-swap", version)]
@@ -8,6 +12,19 @@ use owo_colors::OwoColorize;
 ///
 /// Longer tool help and discussion goes here. This can be multiple lines.
 pub struct Args {
+    /// Run the game directly, without SKSE.
+    #[clap(long, short, global = true)]
+    no_skse: bool,
+    /// Be more chatty about what the tool is doing.
+    #[clap(long, short, global = true)]
+    verbose: bool,
+    /// Be not chatty at all about what the tool is doing.
+    #[clap(long, short, global = true)]
+    quiet: bool,
+    /// Optionally, the game directory to target. Defaults to the directory the tools is in.
+    #[clap(long, short, global = true, default_value = ".")]
+    gamedir: String,
+    /// What to do.
     #[clap(subcommand)]
     cmd: Command,
 }
@@ -20,20 +37,168 @@ pub enum Command {
     Run { version: String },
 }
 
+/// Set up tee-ed logging to console and to a log file unless requested not to.
+fn initialize_logging(args: &Args) -> Result<(), Report> {
+    let level = if args.verbose {
+        // Debug-level logging should be designed for players to read when they
+        // are trying to debug problems.
+        LevelFilter::Debug
+    } else if args.quiet {
+        // Error- and warn-level logging should be designed to inform players about truly important
+        // problems or results.
+        LevelFilter::Warn
+    } else {
+        // Info-level should be designed for players to read normally.
+        LevelFilter::Info
+    };
+
+    // Shown in terminal.
+    let config = simplelog::ConfigBuilder::new()
+        .set_time_level(LevelFilter::Off)
+        .set_thread_level(LevelFilter::Off)
+        .set_module_level(LevelFilter::Off)
+        .set_target_level(LevelFilter::Off)
+        .set_enable_paris_formatting(true)
+        .set_level_padding(simplelog::LevelPadding::Right)
+        .build();
+    // Logged to file.
+    let writeconfig = simplelog::ConfigBuilder::new()
+        .set_enable_paris_formatting(false)
+        .set_module_level(LevelFilter::Off)
+        .set_target_level(LevelFilter::Off)
+        .set_time_format_rfc3339()
+        .set_time_offset_to_local() // this is fallible, so...
+        .unwrap_or_else(|xs| xs)
+        .build();
+
+    CombinedLogger::init(vec![
+        TermLogger::new(level, config, TerminalMode::Mixed, ColorChoice::Auto),
+        WriteLogger::new(
+            LevelFilter::Debug,
+            writeconfig,
+            File::options()
+                .create(true)
+                .append(true)
+                .open("version-swap.log")?,
+        ),
+    ])?;
+    Ok(())
+}
+
+/// Collect relevant files in the given subdirectory of Versions, including
+/// any plain files in a `data` subdirectory.
+fn files_to_copy(dirname: &PathBuf) -> Result<Vec<PathBuf>> {
+    let files: Vec<PathBuf> = std::fs::read_dir(dirname)?
+        .filter_map(|xs| {
+            let Ok(entry) = xs else {
+                return None;
+            };
+            if entry.path().is_dir() {
+                if entry.file_name().eq_ignore_ascii_case("data") {
+                    // Note the bug.
+                    Some(files_to_copy(&entry.path()).unwrap_or_default())
+                } else {
+                    None
+                }
+            } else {
+                Some(vec![entry.path()])
+            }
+        })
+        .flatten()
+        .collect();
+
+    Ok(files)
+}
+
+fn check_setup(args: &Args) -> Result<(), Report> {
+    log::info!("Checking the setup in <b><green>{}</>", args.gamedir);
+
+    // collect directories matching the pattern `skyrim_*` in "{args.gamedir}/Versions"
+    let version_dir = PathBuf::from(format!("{}/Versions", args.gamedir));
+    let versions: Vec<(PathBuf, String)> = std::fs::read_dir(version_dir)?
+        .filter_map(|entry| {
+            let Ok(dir) = entry else {
+                return None;
+            };
+            if !dir.path().is_dir() {
+                return None;
+            }
+            let binding = dir.file_name();
+            let lossy_fname = binding.to_string_lossy();
+
+            if lossy_fname.starts_with("skyrim-") {
+                let vstr = lossy_fname.replace("skyrim-", "");
+                Some((dir.path(), vstr))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let required = vec![
+        "SkyrimSE.exe",
+        "skse64_loader.exe",
+        "steam_api64.dll",
+        "data/ccBGSSSE001-Fish.esm",
+        "data/_ResourcePack.esl",
+        // "skse64_steam_loader.dll",
+    ];
+
+    for (version_dir, version_string) in versions {
+        let mut version_good = true;
+        log::info!("Checking game version <blue><bold>{version_string}</>");
+        let files = files_to_copy(&version_dir)?;
+        for mandatory in required.as_slice() {
+            if files.contains(&PathBuf::from(format!(
+                "{}/{mandatory}",
+                version_dir.display()
+            ))) {
+                log::debug!("    ✔️  <b>{mandatory}</> found");
+            } else {
+                version_good = false;
+                log::warn!("    ⚠️  <b>{mandatory}</> missing");
+            }
+        }
+
+        let skse_dll = format!("skse64_{}.dll", version_string.replace(".", "_"));
+        let skse_expected = format!(
+            "{}/skse64_{}.dll",
+            args.gamedir,
+            version_string.replace(".", "_")
+        );
+        if PathBuf::from(&skse_expected).exists() {
+            log::debug!("    ✔️  <b>{skse_dll}</> found");
+        } else {
+            log::warn!(" ⚠️  missing <b>{skse_dll}</>");
+            version_good = false;
+        }
+
+        if version_good {
+            log::info!("SkyrimSE <blue><bold>{version_string}</> ready for swapping.\n");
+        } else {
+            log::warn!("Problems found with <blue><bold>skyrim-{version_string}</>!")
+        }
+    }
+
+    Ok(())
+}
+
+fn run_version(version: &str, _args: &Args) -> Result<(), Report> {
+    log::info!(
+        "You ran `<blue>version-swap run <b>{}</>`. We would set up that version then run SKSE.",
+        version
+    );
+    log::warn!("NOT IMPLEMENTED YET");
+    Ok(())
+}
+
 fn main() -> Result<(), Report> {
     let args = Args::parse();
+    initialize_logging(&args)?;
+
     match args.cmd {
-        Command::Check => println!(
-            "You ran `{}`. We would check that your game directory is set up properly.",
-            "version-swap check".blue()
-        ),
-        Command::Run { version } => {
-            println!(
-                "You ran `{} {}`. We would set up that version then run SKSE.",
-                "version-swap run".blue(),
-                version.blue().bold()
-            );
-        }
+        Command::Check => check_setup(&args)?,
+        Command::Run { ref version } => run_version(version.as_str(), &args)?,
     }
 
     Ok(())
